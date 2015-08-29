9d76b1d15bbc769848df4fee018afb4280de1dd0
Dalou <autrusseau.damien@gmail.com>
2015-08-29 21:24:08 +0200

    minor fix

--- fields_bundle/forms/images.py
+++ fields_bundle/forms/images.py
@@ -18,52 +18,57 @@ except:
 
 logger = logging.getLogger(__name__)
 
+
+
 class ImageInput(OriginalFileInput):
-    """
-    Widget prodiving a input element for file uploads based on the
-    Django ``FileInput`` element. It hides the actual browser-specific
-    input element and shows the available image for images that have
-    been previously uploaded. Selecting the image will open the file
-    dialog and allow for selecting a new or replacing image file.
-    """
     template_name = 'fields_bundle/_image_input.html'
-    attrs = { 'accept': 'image/*' }
 
-    def render(self, name, value, attrs=None):
-        """
-        Render the ``input`` field based on the defined ``template_name``. The
-        image URL is take from *value* and is provided to the template as
-        ``image_url`` context variable relative to ``MEDIA_URL``. Further
-        attributes for the ``input`` element are provide in ``input_attrs`` and
-        contain parameters specified in *attrs* and *name*.
-        If *value* contains no valid image URL an empty string will be provided
-        in the context.
-        """
-        if value is None:
-            value = ''
+# class ImageInput(OriginalFileInput):
+#     """
+#     Widget prodiving a input element for file uploads based on the
+#     Django ``FileInput`` element. It hides the actual browser-specific
+#     input element and shows the available image for images that have
+#     been previously uploaded. Selecting the image will open the file
+#     dialog and allow for selecting a new or replacing image file.
+#     """
+#     template_name = 'fields_bundle/_image_input.html'
+#     attrs = { 'accept': 'image/*' }
 
-        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
-        if value != '':
-            # Only add the 'value' attribute if a value is non-empty.
-            final_attrs['value'] = force_unicode(self._format_value(value))
+#     def render(self, name, value, attrs=None):
+#         """
+#         Render the ``input`` field based on the defined ``template_name``. The
+#         image URL is take from *value* and is provided to the template as
+#         ``image_url`` context variable relative to ``MEDIA_URL``. Further
+#         attributes for the ``input`` element are provide in ``input_attrs`` and
+#         contain parameters specified in *attrs* and *name*.
+#         If *value* contains no valid image URL an empty string will be provided
+#         in the context.
+#         """
+#         if value is None:
+#             value = ''
 
-        image_url = final_attrs.get('value', '')
-        image_original_url = None
-        image_thumb = None
-        if image_url:
-            image_original_url = os.path.join(settings.MEDIA_URL, image_url)
-            try:
-                image_thumb = get_thumbnail(image_url, 'x100', crop='center', upscale=True)
-            except IOError as inst:
-                logger.error(inst)
+#         final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+#         if value != '':
+#             # Only add the 'value' attribute if a value is non-empty.
+#             final_attrs['value'] = force_unicode(self._format_value(value))
 
-        return render_to_string(self.template_name, Context({
-            'image_thumb': image_thumb,
-            'input_attrs': flatatt(final_attrs),
-            'image_url': image_url,
-            'image_original_url': image_original_url,
-            'image_id': "%s-image" % final_attrs['id'],
-            'name': "%s" % final_attrs['name'],
-        }))
+#         image_url = final_attrs.get('value', '')
+#         image_original_url = None
+#         image_thumb = None
+#         if image_url:
+#             image_original_url = os.path.join(settings.MEDIA_URL, image_url)
+#             try:
+#                 image_thumb = get_thumbnail(image_url, 'x100', crop='center', upscale=True)
+#             except IOError as inst:
+#                 logger.error(inst)
+
+#         return render_to_string(self.template_name, Context({
+#             'image_thumb': image_thumb,
+#             'input_attrs': flatatt(final_attrs),
+#             'image_url': image_url,
+#             'image_original_url': image_original_url,
+#             'image_id': "%s-image" % final_attrs['id'],
+#             'name': "%s" % final_attrs['name'],
+#         }))
 
 
