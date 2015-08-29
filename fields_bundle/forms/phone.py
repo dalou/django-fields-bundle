fec0bf0aafc251ddb8b4aa62b2959a8c8c7ef074
Dalou <autrusseau.damien@gmail.com>
2015-08-29 20:02:32 +0200

    fix modules imports

--- fields_bundle/forms/phone.py
+++ fields_bundle/forms/phone.py
@@ -1,11 +1,11 @@
-# encoding: utf-8
+# # encoding: utf-8
 
-from django import forms
-from django.utils.translation import ugettext_lazy as _
+# from django import forms
+# from django.utils.translation import ugettext_lazy as _
 
-from localflavor.fr.forms import FRPhoneNumberField as OriginalFRPhoneNumberField
+# from localflavor.fr.forms import FRPhoneNumberField as OriginalFRPhoneNumberField
 
-class FRPhoneNumberField(OriginalFRPhoneNumberField):
-    default_error_messages = {
-        'invalid': _(u"Le numéro de téléphone doit correspondre au format 0X XX XX XX XX."),
-    }
+# class FRPhoneNumberField(OriginalFRPhoneNumberField):
+#     default_error_messages = {
+#         'invalid': _(u"Le numéro de téléphone doit correspondre au format 0X XX XX XX XX."),
+#     }
