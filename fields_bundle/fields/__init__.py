e1b78b051c00ed6963f5621b3d64751951d34930
Dalou <autrusseau.damien@gmail.com>
2015-09-05 12:16:06 +0200

    Add PriceField

--- fields_bundle/fields/__init__.py
+++ fields_bundle/fields/__init__.py
@@ -1,4 +1,5 @@
 from .uuid_field import UUIDField
 from .colors import ColorField
 from .icons import IconField
+from .prices import PriceField
 from .files import ContentTypeRestrictedFileField, UniqueFilename, unique_filename
