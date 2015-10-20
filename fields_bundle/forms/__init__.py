ab2da6cd1de06bcd28f3595ac40db14e0bff4299
Dalou <autrusseau.damien@gmail.com>
2015-10-20 10:22:23 +0200

    add default tinyMCE from tinymce package

--- fields_bundle/forms/__init__.py
+++ fields_bundle/forms/__init__.py
@@ -3,3 +3,5 @@ from .icons import IconField, IconInput
 from .prices import PriceField, PriceInput
 from .images import ImageInput, CroppedImageField, CroppedImageInput
 from .dates import DateField, DateTimeField, DateTimeInput, DateInput
+
+from tinymce.widgets import TinyMCE as TinyMCEInput
--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
     install_requires=[
         'django >= 1.8.4, <= 1.9',
         "jsonfield == 1.0.3",
-        "babel == 1.3"
+        "babel == 1.3",
+        "django-tinymce == 2.0.5"
     ],
 )
