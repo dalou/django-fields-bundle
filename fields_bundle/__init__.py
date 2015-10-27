29dc94482fd628a8a7c00db4129ac79ef547f2d6
Dalou <autrusseau.damien@gmail.com>
2015-10-27 20:09:51 +0100

    fix version finder

---
+++ fields_bundle/__init__.py
@@ -0,0 +1,9 @@
+default_app_config = 'fields_bundle.DefaultConfig'
+
+from django.apps import AppConfig
+
+class DefaultConfig(AppConfig):
+    name = 'fields_bundle'
+    verbose_name = u"Fields bundle"
+
+
