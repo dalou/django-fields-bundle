bbb6a5b0be35c417a5568f1ffdc2e0a115899883
Dalou <autrusseau.damien@gmail.com>
2015-11-17 12:49:04 +0100

    fix setup __init__ exception

--- fields_bundle/__init__.py
+++ fields_bundle/__init__.py
@@ -1,9 +1,13 @@
-default_app_config = 'fields_bundle.DefaultConfig'
+try:
+    default_app_config = 'fields_bundle.DefaultConfig'
 
-from django.apps import AppConfig
+    from django.apps import AppConfig
 
-class DefaultConfig(AppConfig):
-    name = 'fields_bundle'
-    verbose_name = u"Fields bundle"
+    class DefaultConfig(AppConfig):
+        name = 'fields_bundle'
+        verbose_name = u"Fields bundle"
+except:
+    # Prevent from package setup
+    pass
 
 
